c8c8c82f26112d41c5591bd4c4d64bcc2e15482b
Paul Michalik <paul.michalik@navvis.com>
2020-02-24 17:24:24 +0100

    Add install and upgrade scripts

---
+++ internal/UpgradeAll.ps1
@@ -0,0 +1,5 @@
+choco upgrade -y cmake.install --install-arguments='"ADD_CMAKE_TO_PATH=System"'
+
+choco upgrade -y git.install -params "/NoShellIntegration /GitOnlyOnPath /WindowsTerminal /SChannel"
+
+choco upgrade -y all
