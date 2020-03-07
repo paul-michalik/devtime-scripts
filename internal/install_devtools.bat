3bc74073d25fac854f323f136b924584fe9a4d3a
Paul Michalik <paul.michalik@outlook.com>
2020-03-07 13:59:10 +0100

    Add linebreaks

--- internal/install_devtools.bat
+++ internal/install_devtools.bat
@@ -1,5 +1,9 @@
-choco install -y cmake.install --install-arguments='"ADD_CMAKE_TO_PATH=System"'
-
-choco install -y git.install -params "/NoShellIntegration /GitOnlyOnPath /WindowsTerminal /SChannel"
-
-choco install -y tortoisegit GitVersion.Portable GitDepend.Portable previewconfig winmerge docker-desktop awscli jq zip unzip
+choco install -y cmake.install --install-arguments='"ADD_CMAKE_TO_PATH=System"' ^
+ & choco install -y git.install -params "/NoShellIntegration /GitOnlyOnPath /WindowsTerminal /SChannel" ^
+ & choco install -y ^
+ tortoisegit ^
+ GitVersion.Portable GitDepend.Portable ^
+ previewconfig ^
+ winmerge ^
+ docker-desktop ^
+ awscli jq zip unzip
--- internal/upgrade_devtools.bat
+++ internal/upgrade_devtools.bat
@@ -1,5 +1,3 @@
-choco upgrade -y cmake.install --install-arguments='"ADD_CMAKE_TO_PATH=System"'
-
-choco upgrade -y git.install -params "/NoShellIntegration /GitOnlyOnPath /WindowsTerminal /SChannel"
-
-choco upgrade -y all
+choco upgrade -y cmake.install --install-arguments='"ADD_CMAKE_TO_PATH=System"' ^
+ & choco upgrade -y git.install -params "/NoShellIntegration /GitOnlyOnPath /WindowsTerminal /SChannel" ^
+ & choco upgrade -y all
