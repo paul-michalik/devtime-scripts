9d2be350d5c74512093c7dba19378de8f69125c2
Paul Michalik <paul.michalik@outlook.com>
2020-03-13 11:52:49 +0100

    Fix conditional execution

--- internal/install_devtools.bat
+++ internal/install_devtools.bat
@@ -1,6 +1,6 @@
 choco install -y cmake.install --install-arguments='"ADD_CMAKE_TO_PATH=System"' ^
- & choco install -y git.install -params "/NoShellIntegration /GitOnlyOnPath /WindowsTerminal /SChannel" ^
- & choco install -y ^
+ && choco install -y git.install -params "/NoShellIntegration /GitOnlyOnPath /WindowsTerminal /SChannel" ^
+ && choco install -y ^
  tortoisegit ^
  GitVersion.Portable GitDepend.Portable ^
  previewconfig ^
