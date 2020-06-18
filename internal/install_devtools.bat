9b70b759be899ae9f6ee915d50916b72a67fccac
Paul Michalik <paul.michalik@navvis.com>
2020-06-18 15:00:17 +0200

    Add VSCode

--- internal/install_devtools.bat
+++ internal/install_devtools.bat
@@ -1,5 +1,6 @@
 choco install -y cmake.install --install-arguments='"ADD_CMAKE_TO_PATH=System"' ^
  && choco install -y git.install -params "/NoShellIntegration /GitOnlyOnPath /WindowsTerminal /SChannel" ^
+ && choco install -y vscode.install --params "/NoDesktopIcon /NoContextMenuFiles /NoContextMenuFolders /DontAssociateWithFiles /NoQuicklaunchIcon" ^
  && choco install -y ^
  tortoisegit ^
  GitVersion.Portable GitDepend.Portable ^
--- internal/upgrade_devtools.bat
+++ internal/upgrade_devtools.bat
@@ -1,3 +1,4 @@
 choco upgrade -y cmake.install --install-arguments='"ADD_CMAKE_TO_PATH=System"' ^
  && choco upgrade -y git.install -params "/NoShellIntegration /GitOnlyOnPath /WindowsTerminal /SChannel" ^
+ && choco upgrade -y vscode.install --params "/NoDesktopIcon /NoContextMenuFiles /NoContextMenuFolders /DontAssociateWithFiles /NoQuicklaunchIcon" ^
  && choco upgrade -y all
