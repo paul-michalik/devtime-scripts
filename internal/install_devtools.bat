8221ffe2898294fc868e15a0129408f71397c788
Paul Michalik <paul.michalik@outlook.com>
2021-03-12 11:50:49 +0100

    1) Update registry in case Git was updated and broke the key 2) Add bash.exe to from Git to 'Path' if not already there 3) Add RefreshEnv

--- internal/install_devtools.bat
+++ internal/install_devtools.bat
@@ -8,4 +8,19 @@ choco install -y cmake.install --install-arguments='"ADD_CMAKE_TO_PATH=System"' 
  docker-desktop ^
  awscli jq zip unzip
 
+REM Apply registry configurtaion to enable usage of bash scripts. This changes every time git is updated! 
+echo Invoking: reg import %~dp0sh_auto_file.reg
+reg import "%~dp0sh_auto_file.reg"
+
+REM Add 'C:\Program Files\Git\bin' to system 'Path' to get 'bash' from Git
+if "%Path%"=="%Path:C:\Program Files\Git\bin;=%" (
+	echo C:\Program Files\Git\bin; not contained in Path, adding
+	setx Path /m "C:\Program Files\Git\bin;%Path%"
+) else (
+	echo C:\Program Files\Git\bin; already contained in Path, do nothing
+)
+
+REM Update current shell with modified environment variables
+RefreshEnv
+
 REM choco install -y vscode.install --params "/NoDesktopIcon /NoContextMenuFiles /NoContextMenuFolders /DontAssociateWithFiles /NoQuicklaunchIcon"
--- internal/upgrade_devtools.bat
+++ internal/upgrade_devtools.bat
@@ -1,5 +1,22 @@
+@echo off
+
 choco upgrade -y cmake.install --install-arguments='"ADD_CMAKE_TO_PATH=System"' ^
- && choco upgrade -y git.install -params "/NoShellIntegration /GitOnlyOnPath /WindowsTerminal /SChannel" ^
- && choco upgrade -y all
+  && choco upgrade -y git.install -params "/NoShellIntegration /GitOnlyOnPath /WindowsTerminal /SChannel" ^
+  && choco upgrade -y all
+
+REM Apply registry configurtaion to enable usage of bash scripts. This changes every time git is updated! 
+echo Invoking: reg import %~dp0sh_auto_file.reg
+reg import "%~dp0sh_auto_file.reg"
+
+REM Add 'C:\Program Files\Git\bin' to system 'Path' to get 'bash' from Git
+if "%Path%"=="%Path:C:\Program Files\Git\bin;=%" (
+	echo C:\Program Files\Git\bin; not contained in Path, adding
+	setx Path /m "C:\Program Files\Git\bin;%Path%"
+) else (
+	echo C:\Program Files\Git\bin; already contained in Path, do nothing
+)
+
+REM Update current shell with modified environment variables
+RefreshEnv
 
 REM  && choco upgrade -y vscode.install --params "/NoDesktopIcon /NoContextMenuFiles /NoContextMenuFolders /DontAssociateWithFiles /NoQuicklaunchIcon" ^
